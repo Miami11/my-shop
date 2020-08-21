b2de944802a9e35134aa2c9343560768334132ce
Mia Chen <ya.ting.chen110@gmail.com>
2020-08-22 00:09:34 +0800

    add content and image to index

--- src/components/public/Main.vue
+++ src/components/public/Main.vue
@@ -9,71 +9,33 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <!-- <svg
-              class="bd-placeholder-img"
-              width="100%"
-              height="100%"
-              xmlns="@/assets/images/banner.jpg"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              role="img"
-          >-->
           <img src="@/assets/images/banner.jpg" alt />
-          <rect width="100%" height="100%" fill="#777" />
-          <!-- </svg> -->
+          <rect width="120%" height="120%" fill="#777" />
+
           <div class="container">
             <div class="carousel-caption text-left">
               <h1>Example headline.</h1>
               <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p>
-                <a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a>
-              </p>
             </div>
           </div>
         </div>
         <div class="carousel-item">
-          <!-- <svg
-              class="bd-placeholder-img"
-              width="100%"
-              height="100%"
-              xmlns="@/assets/images/banner2.jpg"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              role="img"
-          >-->
           <img src="@/assets/images/banner2.jpg" alt />
-          <rect width="100%" height="100%" fill="#777" />
-          <!-- </svg> -->
+
           <div class="container">
             <div class="carousel-caption">
               <h1>Another example headline.</h1>
               <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p>
-                <a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a>
-              </p>
             </div>
           </div>
         </div>
         <div class="carousel-item">
-          <!-- <svg
-              class="bd-placeholder-img"
-              width="100%"
-              height="100%"
-              xmlns="@/assets/images/banner3                    .jpg"
-              preserveAspectRatio="xMidYMid slice"
-              focusable="false"
-              role="img"
-          >-->
           <img src="@/assets/images/banner3.jpg" alt />
-          <rect width="100%" height="100%" fill="#777" />
-          <!-- </svg> -->
+
           <div class="container">
             <div class="carousel-caption text-right">
-              <h1>One more for good measure.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p>
-                <a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a>
-              </p>
+              <h1>專業種植經驗</h1>
+              <p>城市小農選擇優質並富基肥的德國進口泥炭土，不僅保肥力佳、清潔乾淨無臭味、且不含病菌害蟲，對初學者來說好用且便利，適用於種植蔬菜、育苗、扦插與盆栽</p>
             </div>
           </div>
         </div>
@@ -94,29 +56,32 @@
       <div class="container marketing">
         <div class="row">
           <div class="col-lg-12">
-            <h2 class="product-title">園藝花</h2>
+            <h2 class="product-title">造景花</h2>
 
             <hr />
           </div>
-          <div class="col-md-3 col-sm-4 col-xs-12" v-for="item in products" :key="item.id">
+          <div
+            class="col-md-3 col-sm-4 col-xs-12 pd-bottom"
+            v-for="item in products"
+            :key="item.id"
+          >
             <div class="group-product">
               <div class="pic">
                 <img class="image-position product-size" :src="item.imageUrl" alt />
-                <!-- <span class="discount">-20%</span> -->
               </div>
 
               <div class="text-product">
                 <p>
-                  <a href>{{item.title}}</a>
+                  <router-link class :to="'show/'+`${item.id}`">{{item.title}}</router-link>
                   <br />
                   <span class="unit">單位:{{ item.unit }}</span>
                 </p>
                 <span
                   class="origin_price line-through"
-                  v-if="item.price"
-                >原價 {{ item.origin_price }} 元</span>
+                  v-if="item.origin_price"
+                >建議售價 {{ item.origin_price }} 元</span>
                 <br />
-                <span class="price" v-if="item.price">售價 {{ item.price }} 元</span>
+                <span class="price" v-if="item.price">特價 {{ item.price }}</span>
                 <a class="add-cart" @click.prevent="addToCart(item.id)" type="button">
                   <i class="fas fa-cart-plus"></i>
                   <i v-if="status.loadingItem === item.id" class="fas fa-spinner fa-spin"></i>
@@ -124,11 +89,91 @@
               </div>
             </div>
           </div>
+          <p>
+            <a class="group-product" href>看更多其他...</a>
+          </p>
         </div>
       </div>
     </div>
 
     <!-- services  -->
+    <div class="container marketing">
+      <hr class="featurette-divider" />
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">
+            造景花
+            <span class="text-muted">優秀的工作團隊</span>
+          </h2>
+          <p class="lead">
+            成為花藝設計師之前，平日裡喜歡蒔花弄草，與花草為伴、品味生活。隨著時間推移，「花藝」已漸漸成為她的陽光、空氣、水，是她生命中不可或缺的重要存在。
+            <br />相信世上所有花草都擁有美麗的靈魂，我們認真且輕柔地對待，從修剪、搭配、組合，所有細節無不精心設計，每一個作品都蘊含了花藝師想傳遞的精神與意涵。我們不吝惜付出時間與心思，只願為你築起一座美麗且值得探索的花園。
+          </p>
+          <p>
+            <a class="btn btn-lg btn-primary" href="#" role="button">了解更多</a>
+          </p>
+        </div>
+        <div class="col-md-5">
+          <div class="side-pic">
+            <img src="@/assets/images/side-bg-1.png" alt />
+          </div>
+        </div>
+      </div>
+
+      <hr class="featurette-divider" />
+
+      <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">
+            專業種植
+            <span class="text-muted">專業材料</span>
+          </h2>
+          <p
+            class="lead"
+          >城市小農選擇優質並富基肥的德國進口泥炭土，不僅保肥力佳、清潔乾淨無臭味、且不含病菌害蟲，對初學者來說好用且便利，適用於種植蔬菜、育苗、扦插與盆栽等多種用途，倒入植栽盒中便可直接使用，亦或與舊土混合使用。</p>
+          <p>
+            <a class="btn btn-lg btn-primary" href="#" role="button">了解更多</a>
+          </p>
+        </div>
+        <div class="col-md-5 order-md-1">
+          <div class="side-pic">
+            <img src="@/assets/images/side-bg-2.png" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="message">
+      <div class="container marketing pd-bottom-ms">
+        <hr class="featurette-divider" />
+
+        <div class="row featurette justify-content-center align-items-center">
+          <form>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="name">姓名</label>
+                <input type="text" class="form-control" id="inputPassword4" placeholder="name" />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="inputAddress">住址</label>
+              <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
+            </div>
+
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">留言</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">送出</button>
+          </form>
+        </div>
+      </div>
+    </div>
     <hr class="featurette-divider" />
     <!-- FOOTER -->
     <footer class="container">
@@ -173,42 +218,23 @@ export default {
         loadingItem: false,
       },
       products: [],
-      product: {},
-      pagination: [],
+
       carts: {},
-      code: "",
-      form: {
-        user: {
-          name: "",
-          email: "",
-          tel: "",
-          address: "",
-        },
-        message: "",
-      },
     };
   },
   methods: {
-    getProducts(page = 1) {
+    getProducts() {
       this.isLoading = true;
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products?page=${page}`;
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
       this.$http.get(api).then((response) => {
-        this.products = response.data.products;
-        this.pagination = response.data.pagination;
-        console.log("response==", response.data);
+        this.products = response.data.products.filter(
+          (product) => product.category == "造景"
+        );
+
         this.isLoading = false;
       });
     },
-    getProduct(id) {
-      this.status.loadingItem = id;
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
-      $("#productModal").modal("show");
-      this.$http.get(api).then((response) => {
-        this.product = response.data.product;
 
-        this.status.loadingItem = "";
-      });
-    },
     addToCart(id, qty = 1) {
       this.status.loadingItem = id;
       const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
@@ -221,9 +247,6 @@ export default {
           },
         })
         .then((response) => {
-          console.log("response add cart==", response.data);
-
-          // this.product.num = 1;
           this.status.loadingItem = "";
           this.getCart();
         });
@@ -235,55 +258,8 @@ export default {
 
       this.$http.get(api).then((response) => {
         this.carts = response.data.data;
-        console.log("response cart==", this.carts);
         this.status.loadingItem = "";
         this.isLoading = false;
-      });
-    },
-    deleteItem(id) {
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart/${id}`;
-      this.$http.delete(api).then((response) => {
-        if (response.suceess) {
-          alert("success");
-        }
-        console.log("response delete==", response);
-        this.getCart();
-      });
-    },
-    useCoupon() {
-      this.isLoading = true;
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/coupon`;
-      let code = this.code;
-      this.$http
-        .post(api, {
-          data: {
-            code,
-          },
-        })
-        .then((response) => {
-          console.log("response coupon==", response);
-          this.getCart();
-          this.isLoading = false;
-        });
-    },
-    createOrder() {
-      const vm = this;
-      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/order`;
-      const order = vm.form;
-      // vm.isLoading = true;
-      this.$validator.validate().then((result) => {
-        if (result) {
-          this.$http.post(url, { data: order }).then((response) => {
-            console.log("訂單已建立", response.data.orderId);
-            // vm.getCart();
-            if (response.data.success) {
-              vm.$router.push(`/customer_checkout/${response.data.orderId}`);
-            }
-            vm.isLoading = false;
-          });
-        } else {
-          console.log("欄位不完整");
-        }
       });
     },
   },
